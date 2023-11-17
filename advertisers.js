238ca13c1d3ea9ed8a2875a8fc31adb7920d28ca
Shivam <sgrgsggg@gmail.com>
2023-11-17 20:30:55 +0530

    "mongodb video"

---
+++ advertisers.js
@@ -0,0 +1,68 @@
+advertisers = [
+  {
+    "_id": 1,
+    "name": "Tech Inc.",
+    "ads": [
+      {
+        "ad_id": 101,
+        "title": "Tech Gadgets Sale",
+        "content": "Get the latest tech gadgets at discounted prices!",
+        "target_criteria": {
+          "interests": ["Technology", "Art"],
+          "gender": "Female"
+        }
+      },
+      {
+        "ad_id": 102,
+        "title": "Tech Accessories",
+        "content": "Upgrade your tech setup with our accessories!",
+        "target_criteria": {
+          "interests": ["Technology"],
+          "gender": "Female"
+        }
+      },
+      {
+        "ad_id": 103,
+        "title": "Smartphone Deals",
+        "content": "Discover great deals on the latest smartphones!",
+        "target_criteria": {
+          "interests": ["Technology"],
+          "gender": "Male"
+        }
+      }
+    ]
+  },
+  {
+    "_id": 2,
+    "name": "Fashion World",
+    "ads": [
+      {
+        "ad_id": 201,
+        "title": "Fashion Sale",
+        "content": "Shop the latest fashion trends now!",
+        "target_criteria": {
+          "interests": ["Fashion"],
+          "gender": "Female"
+        }
+      },
+      {
+        "ad_id": 202,
+        "title": "Men's Fashion",
+        "content": "Discover the latest trends in men's fashion!",
+        "target_criteria": {
+          "interests": ["Fashion", "Movies"],
+          "gender": "Male"
+        }
+      },
+      {
+        "ad_id": 203,
+        "title": "Accessories Collection",
+        "content": "Complete your look with our stylish accessories!",
+        "target_criteria": {
+          "interests": ["Cooking", "Art", "Travel"],
+          "gender": "Female"
+        }
+      }
+    ]
+  }
+]
